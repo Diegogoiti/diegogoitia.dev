24eca887add35dbdca2130a95fcfc60d1047dc28
Diego <diegogoitiazx1@gmail.com>
2026-04-15 14:27:08 -0400

    feat: creo que ya esta lista

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    proyectos_destacados = [
+        {
+            'titulo': 'DYFIT Student Manager (Rust)',
+            'descripcion': 'Gestión de estudiantes para dojo, optimizado con SQLite y seguridad de tipos.',
+            'url': 'https://github.com/Diegogoiti/DYFIT-Student-Manager-dioxus'
+        },
+        {
+            'titulo': 'Esta página :) (Flask + tailwind)',
+            'descripcion': 'Es mi primera página web, seguiremos mejorando con el tiempo. ',
+            'url': '#'
+        }
+    ]
+    return render_template("index.html", proyectos=proyectos_destacados)
+
+if __name__ == '__main__': app.run(host='0.0.0.0', port=5000, debug=True)
